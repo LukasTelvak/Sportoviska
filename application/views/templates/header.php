7919ef586f82fefbb726aaf63875483827f2d08c
LukasTelvak <lukastelvakn.e.x.u.s@gmail.com>
2017-05-22 14:25:23 +0200

    navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,4 +20,18 @@
     <!-- Custom styles for this template -->
     <link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet">
 </head>
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="<?php echo base_url(); ?>home">Sportoviska</a>
+        </div>
+        <div id="navbar">
+            <ul class="nav navbar-nav">
+                <li><a href="<?php echo base_url(); ?>about">About</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
 
